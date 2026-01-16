7b52c7b6c7aa8bef3aba9ca1a3bf2c0bc7272d19
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-01-15 19:26:20 -0800

    Removed unused imports

--- app/streamlit_app/client.py
+++ app/streamlit_app/client.py
@@ -1,12 +1,5 @@
-import asyncio
-import streamlit as st
 from langgraph_sdk import get_client
 from settings import get_settings
-from chat_modes import (
-    OPTIONS,
-    CHAT_OPTIONS,
-    ChatOption,
-)
 
 
 settings = get_settings()
@@ -21,6 +14,7 @@ else:
         url=settings.langgraph_production_url,
         api_key=settings.langsmith_api_key,
     )
+
 
 async def stream_langgraph(messages, thread_id: str | None, assistant_id: str):
     async for chunk in client.runs.stream(
@@ -39,6 +33,6 @@ async def stream_langgraph(messages, thread_id: str | None, assistant_id: str):
         if not messages_list:
             continue
         last_msg = messages_list[-1]
-        print("Last message in stream", last_msg)
+        # print("Last message in stream", last_msg)
         if last_msg:
             yield last_msg
