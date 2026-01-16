728e2b8623191be622ccaf7ab12b78ee99fd9fa9
Luciano Bermudez <lucianobmecheng69@gmail.com>
2026-01-16 13:54:42 -0800

    updated path

--- app/langgraph_server/src/ai_tools/ai_tools.py
+++ app/langgraph_server/src/ai_tools/ai_tools.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from langchain.tools import tool
-from app.core.settings import get_settings
+from core.settings import get_settings
 
 
 output = get_settings().output_path
